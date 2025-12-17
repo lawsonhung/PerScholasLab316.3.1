ed8acca0f78cc3df99da01f149f5d106517cf133
Lawson Hung <lawsonhung@yahoo.com>
2025-12-17 17:31:24 -0500

    Implemented handleTopMenu to console.log e.target

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,27 @@
 // Menu data structure
+// var menuLinks = [
+//   { text: 'about', href: '/about' },
+//   { text: 'catalog', href: '/catalog' },
+//   { text: 'orders', href: '/orders' },
+//   { text: 'account', href: '/account' },
+// ];
+
 var menuLinks = [
-  { text: 'about', href: '/about' },
-  { text: 'catalog', href: '/catalog' },
-  { text: 'orders', href: '/orders' },
-  { text: 'account', href: '/account' },
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
 ];
 
 // ============================================= PART ONE
@@ -59,3 +77,21 @@ subMenuEl.style.height = '100%';
 subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
 
 subMenuEl.classList.add('flex-around');
+
+subMenuEl.style.position = 'absolute';
+
+subMenuEl.style.top = '0';
+
+// ============================================= PART FOUR
+
+let topMenuLinks = document.querySelectorAll('a');
+
+topMenuEl.addEventListener('click', handleTopMenu)
+
+function handleTopMenu(e) {
+  e.preventDefault();
+
+  if (e.target.tagName !== 'A') return;
+
+  console.log(e.target);
+}
